9ea5860a72ff29f7ad4f2ffc04a49dec1fb0c650
HoboG0blin <219255999+HoboG0blin@users.noreply.github.com>
2025-10-30 14:59:58 +0200

    release script pipeline fixes

--- scripts/pathbegone.sh
+++ scripts/pathbegone.sh
@@ -8,5 +8,7 @@ GIT_TOPLEVEL=$(git rev-parse --path-format=relative --show-toplevel)
 FILES="$(grep --files-with-match --exclude-dir "scripts" -R "mods/Bedlam_Skunkworks/" $GIT_TOPLEVEL || true)"
 
 for file in $FILES; do
-    sed -i 's|mods/Bedlam_Skunkworks/||' $file    
+    sed -i 's|mods/Bedlam_Skunkworks/||' $file  
+    git add $file  
+    git commit --amend --no-edit
 done
